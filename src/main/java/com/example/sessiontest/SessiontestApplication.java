8bbc8d510ad52637a3e485754492a0867660a656
김동률 <lancelot1672@naver.com>
2023-07-24 15:06:29 +0900

    git init

---
+++ src/main/java/com/example/sessiontest/SessiontestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sessiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessiontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessiontestApplication.class, args);
+	}
+
+}
